a8e63aaa988f267613b84fca34e7e7e07405fbf6
HoLegolas <hoquandai@gmail.com>
2019-07-05 11:19:47 +0700

    Task A done

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+#...
+  Product.create(
+    title: 'Seven Moblie Apps in Seven Weeks',
+    description:
+      %{<p>
+        <em>Native Apps, Multiple Platforms</em>
+        You'll find out which crsoo-platform solutiion makes 
+        the most sense for your needs.
+        </p>},
+    image_url: '7apps.jpg',
+    price: 26.00)
+#...
