400e64a26c11452b460c43859c4fdfa2ff9bcd68
HoLegolas <hoquandai@gmail.com>
2019-07-08 11:36:07 +0700

    Task E: Finishing the Cart

--- depot/app/controllers/carts_controller.rb
+++ depot/app/controllers/carts_controller.rb
@@ -1,6 +1,8 @@
-class CartsController < ApplicationController
-  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+# frozen_string_literal: true
 
+class CartsController < ApplicationController
+  before_action :set_cart, only: %i[show edit update destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   # GET /carts
   # GET /carts.json
   def index
@@ -9,8 +11,7 @@ class CartsController < ApplicationController
 
   # GET /carts/1
   # GET /carts/1.json
-  def show
-  end
+  def show; end
 
   # GET /carts/new
   def new
@@ -18,8 +19,7 @@ class CartsController < ApplicationController
   end
 
   # GET /carts/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /carts
   # POST /carts.json
@@ -54,21 +54,30 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_index_url, notice: 'Your cart is currently empty.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    # Use callbacks to share common setup or constraints between actions
     def set_cart
       @cart = Cart.find(params[:id])
+    rescue
+      logger.error "Attemp to access invalid cart #{params[:id]}"
+      redirect_to store_index_url, notice: 'Invalid cart'
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params.fetch(:cart, {})
+    end
+
+    def invalid_cart
+      logger.error "Attemp to access invalid cart #{param[:id]}"
+      redirect_to store_index_url, notice: 'Invalid cart'
     end
 end
--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -1,46 +1,49 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
   fixtures :products
 
-  test "product attributes must not be empty" do
-  	product = Product.new
-  	assert product.invalid?
-  	assert product.errors[:title].any?
-  	assert product.errors[:description].any?
-  	assert product.errors[:price].any?
-  	assert product.errors[:image_url].any?
+  test 'product attributes must not be empty' do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
   end
 
-  test "product price must be positive" do
-  	product = Product.new(
-  		title: "My book title",
-  		description: "yyy",
-  		image_url: "zzz.jpg")
+  test 'product price must be positive' do
+    product = Product.new(
+      title: 'My book title',
+      description: 'yyy',
+      image_url: 'zzz.jpg'
+    )
 
-  	product.price = -1
-  	assert product.invalid?
-  	assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    product.price = -1
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
 
     product.price = 0
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
 
     product.price = 1
     assert product.valid?
   end
 
   def new_product(image_url)
-    Product.new(title: "My book title",
-                description: "yyy",
+    Product.new(title: 'My book title',
+                description: 'yyy',
                 price: 1,
                 image_url: image_url)
   end
 
-  test "image url" do
-    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg 
-             http://a.b.c/x/y/z/fred.jpg }
-    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+  test 'image url' do
+    ok = %w[ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c/x/y/z/fred.jpg ]
+    bad = %w[fred.doc fred.gif/more fred.gif.more]
 
     ok.each do |name|
       assert new_product(name).valid?, "#{name} shouldn't be invalid"
@@ -51,11 +54,11 @@ class ProductTest < ActiveSupport::TestCase
     end
   end
 
-  test "product is not valid witout a unique title" do
+  test 'product is not valid witout a unique title' do
     product = Product.new(title: products(:ruby).title,
-                          description: "yyy",
+                          description: 'yyy',
                           price: 1,
-                          image_url: "fred.gif")
+                          image_url: 'fred.gif')
     assert product.invalid?
     assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
