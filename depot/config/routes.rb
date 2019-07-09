bfaa4c0d64fa61ffbb2a9c8890c7033ab622d1e7
HoLegolas <hoquandai@gmail.com>
2019-07-09 16:42:33 +0700

    Task I: Logging in

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
   get 'store/index'
   get 'store/about'
-  
+
   resources :products do
     get :who_bought, on: :member
   end
